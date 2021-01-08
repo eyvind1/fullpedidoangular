bd0592c33e3e513a3447a0e829a1b481c96b2e26
eyvind1 <eyvind.osco@ucsp.edu.pe>
2021-01-08 10:26:39 -0500

    lastUpdate

--- src/app/components/carrusel/carrusel.component.ts
+++ src/app/components/carrusel/carrusel.component.ts
@@ -1,24 +1,31 @@
 import { Component, OnInit,Input, AfterViewInit, OnChanges, SimpleChanges } from '@angular/core';
-import { NgbCarouselConfig, NgbSlideEvent, NgbSlideEventSource } from '@ng-bootstrap/ng-bootstrap';
+//import { NgbCarousel, NgbCarouselConfig, NgbSlideEvent, NgbSlideEventSource } from '@ng-bootstrap/ng-bootstrap';
 import { CategoriasModel } from 'src/app/models/categoria';
 import { CategoriasService } from 'src/app/services/categorias.service';
 import { Swiper } from 'swiper';
 import { FirebaseService } from '../../services/firebase.service';
 import { Autoplay } from 'swiper';
+import { ViewChild } from '@angular/core';
 Swiper.use([Autoplay]);
 
 @Component({
   selector: 'app-carrusel',
   templateUrl: './carrusel.component.html',
-  styleUrls: ['./carrusel.component.css']
+  styleUrls: ['./carrusel.component.css'],
+  //providers: [NgbCarouselConfig]
 })
 export class CarruselComponent implements OnInit, AfterViewInit,OnChanges{
   
   @Input() promociones:any[];
   @Input() categoriaselected:any =1 ;
   public mySwiper: Swiper;
-
-  constructor() {  }
+  //@ViewChild('mycarousel', {static : true}) carousel: NgbCarousel;
+  constructor(
+    //config: NgbCarouselConfig
+    ) 
+    {
+    //config.interval = 2000;
+    }
   ngOnChanges(changes: SimpleChanges): void {
     /* this.promociones_img= this.promociones.filter((data:any)=>(data.emp_cat == this.categoriaselected));
     console.log(this.promociones_img);
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebase : {
     apiKey: "AIzaSyCfv7io1liNeZwLuTwMtph38_jDkut1H0w",
     authDomain: "gmtc-74128.firebaseapp.com",
